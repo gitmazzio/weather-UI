56d596e55af68a6f03f4a24ff77fac2520542a8e
Matteo Mazziotti <matteo@TMT-PRODUCT26.tomatowin.local>
2019-06-02 12:01:21 +0200

    added styles to single badge

--- src/badgeWeather/BadgeWeather.jsx
+++ src/badgeWeather/BadgeWeather.jsx
@@ -5,10 +5,10 @@ class BadgeWeather extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            dayTime: this.props.data.dt,
+            /* dayTime: this.props.data.dt,
             pressure: this.props.data.pressure,
             temp: this.props.data.temp,
-            weather: this.props.data.weather[0]
+            weather: this.props.data.weather[0] */
         }
     }
 
@@ -16,20 +16,42 @@ class BadgeWeather extends React.Component {
         if ()
     } */
 
-    getWeatherHours = () => {
-        const actualDate = new Date(this.state.dayTime * 1000);
-        return actualDate.toLocaleDateString('it-IT');
+    getDay = () => {
+        const actualDate = new Date(this.props.data.dt * 1000).toLocaleDateString('it-IT');
+        const today = new Date();
+        let tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
 
+        if (actualDate === today.toLocaleDateString('it-IT')) {
+            return 'Today'
+        }
+        if (actualDate === tomorrow.toLocaleDateString('it-IT')) {
+            return 'Tomorrow';
+        }
+        else {
+            return actualDate;
+        }
+    }
+
+    getPicUrl = () => {
+
+        return `/png/${this.props.data.weather[0].main.toLowerCase()}.png`
     }
 
     render() {
         return (
-            <div className="badge">
+            <div className="w-badge">
+                <p>{this.getDay()}</p>
+                <img src={this.getPicUrl()} alt="" className="w-img" />
+
                 <h4>
-                    {this.state.weather.main}
+                    {this.props.data.weather.main}
                 </h4>
-                <p>{this.state.weather.description}</p>
-                <p>{this.getWeatherHours()}</p>
+                <p>{this.props.data.weather.main}</p>
+                <p>
+                    <span className="max">{Math.ceil(this.props.data.temp.max)}°C</span>
+                    <span className="min">{Math.floor(this.props.data.temp.min)}°C</span>
+                </p>
             </div>
         );
     }
